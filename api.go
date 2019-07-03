a87792d71eba2fa7f054a896e531437623db58ef
xiangxu <iasonliu@gmail.com>
2019-07-03 17:43:18 +0800

    add data struct

--- api.go
+++ api.go
@@ -1,1 +1,8 @@
 package nap
+
+type API struct {
+	BaseURL       string // https://httpbin.org
+	Resources     map[string]RestResource
+	DefaultRouter *CBRouter
+	Client        *Client
+}
--- resource.go
+++ resource.go
@@ -1,1 +1,7 @@
 package nap
+
+type RestResource struct {
+	Endpoint string // /get/{.userid} user=tom
+	Method   string // GET
+	Router   *CBRouter
+}
